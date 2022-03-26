92216e4f9641ce021c3900f906f6075272ef27d0
Roberto Sánchez Arévalo <rsa568422@gmail.com>
2022-03-26 03:14:09 +0100

    Clase 15

--- endpoints/posts/index.spec.js
+++ endpoints/posts/index.spec.js
@@ -1,7 +1,32 @@
+const postHandlers = require('./index')
+
 describe('Endpoints', () => {
     describe('posts', () => {
-        it.skip('should create', () => ({
+        it('should create', async () => {
+            const mockUsers = [ { id:1 }, { id:2 } ]
+            const post = {
+                userId: 1,
+                tittle: 'Título',
+                body: 'Cuerpo del post'
+            }
+            const req = {
+                body: post
+            }
+            const res = {
+                status: jest.fn().mockReturnThis(),
+                send: jest.fn()
+            }
+            const axios = {
+                get: jest.fn().mockResolvedValue({ data: mockUsers }),
+                post: jest.fn().mockResolvedValue({ data: { id:1000 } })
+            }
 
-        }))
+            await postHandlers({ axios }).post(req, res)
+
+            expect(res.status.mock.calls).toEqual([ [201] ])
+            expect(res.send.mock.calls).toEqual([ [{ id:1000 }] ])
+            expect(axios.get.mock.calls).toEqual([ ['https://jsonplaceholder.typicode.com/users'] ])
+            expect(axios.post.mock.calls).toEqual([ ['https://jsonplaceholder.typicode.com/posts', post] ])
+        })
     })
 })
