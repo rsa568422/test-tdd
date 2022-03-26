bf4da55f969964b153e28731d806b49c7ab1849f
Roberto Sánchez Arévalo <rsa568422@gmail.com>
2022-03-26 04:21:43 +0100

    Clase 20

--- endpoints/posts/index.test.js
+++ endpoints/posts/index.test.js
@@ -15,6 +15,16 @@ describe('Server', () => {
                 expect(response.body.userId).toEqual(5)
                 expect(response.body).toHaveProperty('id')
             })
+
+            it('does not creates a new post', async () => {
+                const response = await request(app)
+                    .post('/')
+                    .send({ userId: 100 })
+                    .set('user_id', 1)
+                    .set('Content-Type', 'application/json')
+
+                expect(response.statusCode).toEqual(400)
+            })
         })
     })
 })
